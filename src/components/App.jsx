a1bab590d2ec062080ffe4dc51e404eba3a8c075
Anatolii Medvediev <med.anatolii@gmail.com>
2022-05-15 02:47:59 +0300

    fix deploy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Layout } from './Layout/Layout';
 import { Toaster } from 'react-hot-toast';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ContactsPage from 'ViewPages/ContactsPage/ContactsPage';
+import { ContactsPage } from 'ViewPages/ContactsPage/ContactsPage';
 import HomePage from 'ViewPages/HomePage/HomePage';
 import LoginPage from 'ViewPages/LoginPage/LoginPage';
 import RegisterPage from 'ViewPages/RegisterPage/RegisterPage';
